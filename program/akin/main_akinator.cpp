87cbebf95865820dde136a8cbc5918e73d2a3b06
Arman <mesterio2003@mail.ru>
2021-11-19 15:04:35 +0300

    end_mb

---
+++ program/akin/main_akinator.cpp
@@ -0,0 +1,33 @@
+#include "akinator.h"
+
+int main ()
+{
+    Tree *tree_node = {};
+    tree_ctor (&tree_node);
+
+    char* buffer = make_tree_from_library (tree_node);
+    
+    char** game_answers = NULL;
+    if (ctr_array_answers_game (&game_answers) == -1)
+    {
+        return -1;
+    }
+
+    game (tree_node, game_answers);
+
+    //definition_tree_elem (tree_node, "GUSHIN");
+
+    difference_definition (tree_node, "MARAT", "ARMAN");
+
+    tree_dump_visual (tree_node);
+    tree_dump_text (tree_node);
+
+    tree_dtor (&tree_node);
+
+    dtor_array_answers_game (&game_answers);
+
+    free (buffer);
+
+    return 0;
+}
+
